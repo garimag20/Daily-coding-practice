0f219100a3970c61570bb1f5722d7f98aeec5f12
garimag20 <63306805+garimag20@users.noreply.github.com>
2021-05-22 13:05:45 +0530

    Time: 0 ms (100.00%), Space: 13.1 MB (89.02%) - LeetHub

--- maximum-subarray/maximum-subarray.cpp
+++ maximum-subarray/maximum-subarray.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1)
+            return nums[0];
         int csum = nums[0];
-        int osum = nums[0];
+        int bsum = nums[0];
         
-        for(int i = 1; i < nums.size(); i++){
-            csum = max(nums[i], csum+nums[i]);
-            osum = max(csum, osum);
+        for(int i = 1; i < n; i++){
+            csum = max(csum+nums[i], nums[i]);
+            bsum = max(bsum, csum);
         }
-        return osum;
+        return bsum;
     }
 };
