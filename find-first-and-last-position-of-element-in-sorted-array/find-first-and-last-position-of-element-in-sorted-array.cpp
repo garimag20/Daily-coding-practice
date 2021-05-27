9c1278a2598bb9f69c84b9a9e4d1f2b21987a440
garimag20 <63306805+garimag20@users.noreply.github.com>
2021-05-27 20:24:16 +0530

    Time: 4 ms (95.77%), Space: 13.6 MB (57.91%) - LeetHub

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,21 +1,53 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int n = nums.size();
-        if(n == 0 || nums[n-1] < target)
-            return {-1,-1};
-        
-        vector<int>result;
-        auto start = lower_bound(nums.begin(), nums.end(), target);
-        auto end = upper_bound(nums.begin(), nums.end(), target);
-        
-        if(nums[start-nums.begin()] != target)
-            return {-1,-1};
-        
-        else{
-            result.push_back(start-nums.begin());
-            result.push_back(end-nums.begin()-1);
+    int lfind(vector<int>& nums, int target){
+        int s = 0;
+        int e = nums.size()-1;
+        int ans = -1;
+        while(s <= e){
+            int mid = s + (e-s)/2;
+            
+            if(nums[mid] == target){
+                ans = mid;
+                e = mid-1;
+            }
+            else if(nums[mid] > target)
+                e = mid-1;
+            else
+                s = mid+1;
         }
-        return result;
+        return ans;
+    }
+    
+    int rfind(vector<int>& nums, int target){
+        int s = 0;
+        int e = nums.size()-1;
+        int ans = -1;
+        while(s <= e){
+            int mid = s + (e-s)/2;
+            
+            if(nums[mid] == target){
+                ans = mid;
+                s = mid+1;
+            }
+            else if(nums[mid] > target)
+                e = mid-1;
+            else
+                s = mid+1;
+        }
+        return ans;
+    }
+    
+    vector<int> searchRange(vector<int>& nums, int target) {
+        if(nums.size() == 0)
+            return {-1,-1};
+        
+        vector<int> ans;
+        int lans = lfind(nums, target);
+        int rans = rfind(nums, target);
+        
+        ans.push_back(lans);
+        ans.push_back(rans);
+        return ans;
     }
 };
