ede694231a68dcd7c87e65c5ed9de8f0a97a8f5c
garimag20 <63306805+garimag20@users.noreply.github.com>
2021-05-31 21:59:40 +0530

    Time: 232 ms (27.21%), Space: 286.1 MB (36.40%) - LeetHub

---
+++ largest-merge-of-two-strings/largest-merge-of-two-strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string largestMerge(string word1, string word2) {
+        string merge = "";
+        while(word1.length() != 0 && word2.length() != 0){
+            if(word1 >= word2){
+                merge += word1[0];
+                word1 = word1.substr(1);
+            }
+            else{
+                merge += word2[0];
+                word2 = word2.substr(1);
+            }
+        }
+        
+        if(word1.length())
+            merge += word1;
+        if(word2.length())
+            merge += word2;
+        
+        return merge;
+    }
+};
