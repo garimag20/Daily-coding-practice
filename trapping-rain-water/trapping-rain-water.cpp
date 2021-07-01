633c7ef96d31f5c69f4fc1d59a674a2d86126b18
garimag20 <63306805+garimag20@users.noreply.github.com>
2021-07-01 21:17:38 +0530

    Time: 8 ms (53.81%), Space: 14.1 MB (78.43%) - LeetHub

--- trapping-rain-water/trapping-rain-water.cpp
+++ trapping-rain-water/trapping-rain-water.cpp
@@ -4,7 +4,6 @@ public:
         int n = height.size();
         if(n == 0)
             return 0;
-        
         int left[n], right[n];
         
         //fill left array
@@ -12,18 +11,18 @@ public:
         for(int i = 1; i < n; i++){
             left[i] = max(left[i-1], height[i]);
         }
-        //fill right array
+        
+        //right array
         right[n-1] = height[n-1];
         for(int i = n-2; i >= 0; i--){
             right[i] = max(right[i+1], height[i]);
         }
+        
+        //calculate water
         int water = 0;
-        //calculate the amount of water stored
         for(int i = 0; i < n; i++){
-            //min(left[i], right[i])-height[i]
             water += min(left[i], right[i]) - height[i];
         }
-        
         return water;
     }
 };
