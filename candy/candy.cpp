8494a83b30e4e4ead88921a2016801dbf39ac268
garimag20 <63306805+garimag20@users.noreply.github.com>
2021-07-02 20:13:13 +0530

    Time: 20 ms (70.03%), Space: 17.8 MB (36.51%) - LeetHub

--- candy/candy.cpp
+++ candy/candy.cpp
@@ -4,17 +4,19 @@ public:
         int n = ratings.size();
         if(n == 1)
             return 1;
-        vector<int> res(n,1);
-        long long tcandies = 0;
+        vector<int> res(n, 1);
+        //case 1: whether a ratig is greater tha left rating
         for(int i = 1; i < n; i++){
-            if(ratings[i] > ratings[i-1])
+            if(ratings[i] > ratings[i-1]){
                 res[i] = res[i-1]+1;
+            }
         }
-        tcandies = res[n-1];
+        int tcandies = res[n-1];
+        //check for right part
         for(int i = n-2; i >= 0; i--){
-            if(ratings[i] > ratings[i+1])
+            if(ratings[i] > ratings[i+1]){
                 res[i] = max(res[i], res[i+1]+1);
-            
+            }
             tcandies += res[i];
         }
         return tcandies;
