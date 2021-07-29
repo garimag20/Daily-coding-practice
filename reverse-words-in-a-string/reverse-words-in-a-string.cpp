b66ac7704d34d2feaa381675666c548e41879c96
garimag20 <63306805+garimag20@users.noreply.github.com>
2021-07-29 21:56:23 +0530

    Time: 4 ms (88.56%), Space: 20.8 MB (21.66%) - LeetHub

---
+++ reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string res;
+        int i = 0;
+        int n = s.length();
+        
+        while(i < n){
+            //remove spaces
+            while(i<n and s[i] == ' ') i++;
+            if(i >= n) break;
+            int j = i+1;
+            while(j < n and s[j] != ' ') j++;
+            string sub = s.substr(i, j-i);
+            if(res.length() == 0) res = sub;
+            else
+                res = sub + " " + res;
+            i = j+1;
+        }
+        return res;
+    }
+};
