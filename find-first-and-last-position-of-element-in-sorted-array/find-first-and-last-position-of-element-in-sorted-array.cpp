6487d37f0fdfcfc9e0291ab6e043e54002d6baf5
garimag20 <63306805+garimag20@users.noreply.github.com>
2021-09-16 15:04:56 +0530

    Time: 19 ms (5.26%), Space: 13.6 MB (64.33%) - LeetHub

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
-    int lfind(vector<int>& nums, int target){
+    int firstoccur(vector<int>& nums, int target){
         int s = 0;
         int e = nums.size()-1;
         int ans = -1;
+        
         while(s <= e){
             int mid = s + (e-s)/2;
-            
             if(nums[mid] == target){
                 ans = mid;
                 e = mid-1;
             }
-            else if(nums[mid] > target)
-                e = mid-1;
-            else
+            else if(nums[mid] < target)
                 s = mid+1;
+            else
+                e = mid-1;
         }
         return ans;
     }
     
-    int rfind(vector<int>& nums, int target){
+    int lastoccur(vector<int>& nums, int target){
         int s = 0;
         int e = nums.size()-1;
         int ans = -1;
+        
         while(s <= e){
             int mid = s + (e-s)/2;
-            
             if(nums[mid] == target){
                 ans = mid;
                 s = mid+1;
             }
-            else if(nums[mid] > target)
-                e = mid-1;
-            else
+            else if(nums[mid] < target)
                 s = mid+1;
+            else
+                e = mid-1;
         }
         return ans;
     }
@@ -41,13 +41,12 @@ public:
     vector<int> searchRange(vector<int>& nums, int target) {
         if(nums.size() == 0)
             return {-1,-1};
-        
         vector<int> ans;
-        int lans = lfind(nums, target);
-        int rans = rfind(nums, target);
+        int first = firstoccur(nums, target);
+        int last = lastoccur(nums, target);
         
-        ans.push_back(lans);
-        ans.push_back(rans);
+        ans.push_back(first);
+        ans.push_back(last);
         return ans;
     }
 };
